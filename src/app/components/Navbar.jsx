2ecd12078926337623a815fd91493f1a361ccd2c
Francisco Lucena <franciscolucena90@gmail.com>
2023-10-12 16:21:21 -0300

    initial commit v1

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,31 @@
+// components/Navbar.js
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-custom-color-500 p-4">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <Link href="/">Mamuk Training
+          </Link>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/about">Sobre Nosotros
+              </Link>
+            </li>
+            <li>
+              <Link href="/classes"> Clases
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact">Contacto
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Navbar from './components/Navbar'
+import Landing from './components/Landing'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  )
+}
